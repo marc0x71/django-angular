1aed6a9590e28d571198064b4329c8adc5a6fa67
marc0x71 <marc0x71@yahoo.it>
2024-04-15 15:11:50 +0200

    Implementazione GetTemplateView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from api import views
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path("xml", views.XmlViewer.as_view()),
     path("xml2json", views.Xml2JsonViewer.as_view()),
     path("async", views.AsyncView.as_view()),
+    path("template/<str:tpl_name>", views.GetTemplateView.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
 import asyncio
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
 
+from django.http import Http404, HttpResponse
+from django.template import loader
 from django.views import View
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status
 from rest_framework import permissions
 
@@ -25,6 +26,7 @@ class DataViewer(APIView):
         return Response(data, status=status.HTTP_200_OK)
 
 
+# http --print=hbm  http://localhost:8000/api/xml
 class XmlViewer(APIView):
     def get(self, request, *args, **kwargs):
         template = loader.get_template("example.xml")
@@ -37,11 +39,13 @@ class XmlViewer(APIView):
                 {"id": 2, "name": "Pickett Parks"},
             ],
         }
-        return HttpResponse(
-            utils.xml_pretty(template.render(context, request)), status=status.HTTP_200_OK
+        return Response(
+            utils.xml_pretty(template.render(context, request)),
+            status=status.HTTP_200_OK,
         )
 
 
+# http --print=hbm  http://localhost:8000/api/xml2json
 class Xml2JsonViewer(APIView):
     def get(self, request, *args, **kwargs):
         template = loader.get_template("example.xml")
@@ -58,6 +62,7 @@ class Xml2JsonViewer(APIView):
         return Response(utils.xml_to_json(data), status=status.HTTP_200_OK)
 
 
+# http --print=hbm  http://localhost:8000/api/async
 class AsyncView(View):
     async def get(self, request, *args, **kwargs):
         # Perform view logic using await.
@@ -68,3 +73,15 @@ class AsyncView(View):
         ]
         await asyncio.sleep(2)
         return HttpResponse(data, status=status.HTTP_200_OK)
+
+
+# echo '{"first":1,"second":2, "data": [{"id":1,"name":"Mario"},{"id":2,"name":"Luigi"}]}'|http --print=hbm -v post http://localhost:8000/api/template/example
+class GetTemplateView(APIView):
+    def post(self, request: Request, tpl_name, format=None):
+        try:
+            template = loader.get_template(tpl_name + ".xml")
+        except:
+            raise Http404
+
+        data = template.render(request.data, request)
+        return Response(utils.xml_to_json(data), status=status.HTTP_200_OK)
